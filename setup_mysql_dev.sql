bf8596a8e945849986ee7f43e99fd6070e61356a
Amilemia <aminemadah2@gmail.com>
2023-09-19 22:20:53 +0100

    Task 3: MySQL Dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,5 +5,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE OR REPLACE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges to the user hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
