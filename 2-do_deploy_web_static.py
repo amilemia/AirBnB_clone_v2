53ddd1010a67b1197ff42f6501898c64a508ad7d
Amilemia <aminemadah2@gmail.com>
2023-10-06 02:48:14 +0100

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from fabric.api import env, put, run
 from os.path import exists
 
 env.hosts = ['52.3.244.240', '52.91.146.187']
+env.user = 'ubuntu'  # Update with your SSH username
 
 
 def do_deploy(archive_path):
@@ -23,34 +24,40 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
+        # Upload the archive to the /tmp/ directory of the web servers
         put(archive_path, "/tmp/")
-        # Get the base name, file name, dest of the archive
+
+        # Get the base name and file name without extension
         base_name = archive_path.split("/")[-1]
         file_name = base_name.split(".")[0]
-        dest_path = "/data/web_static/releases/{}/".format(file_name)
 
-        # Uncompress the archive to the folder on the web server
+        # Create the destination folder on the web servers
+        dest_path = "/data/web_static/releases/{}/".format(file_name)
         run("mkdir -p {}".format(dest_path))
+
+        # Uncompress the archive to the destination folder
         run("tar -xzf /tmp/{} -C {}".format(base_name, dest_path))
 
-        # Delete the archive from the web server
+        # Delete the archive from the web servers
         run("rm /tmp/{}".format(base_name))
 
-        # Move the files
-        run("mv {0}web_static/* {0}".format(dest_path))
+        # Copy files to the web_static/releases folder instead of moving them
+        run("cp -r {0}web_static/* {0}".format(dest_path))
 
-        # Delete the symbolic link from the web server
-        run("rm -rf {}web_static".format(dest_path))
+        # Delete the symbolic link from the web servers
         run("rm -rf /data/web_static/current")
 
-        # Create a new symbolic link on the web server
+        # Create a new symbolic link on the web servers
         run("ln -s {} /data/web_static/current".format(dest_path))
 
-        # TADA
-        print("New version deployed!")
+        # Check if the link was successfully updated
+        current_link = run("readlink -f /data/web_static/current")
+        if current_link == dest_path:
+            print("New version deployed!")
+            return True
+        else:
+            return False
 
     except Exception:
         return False
-
-    return True
+    
